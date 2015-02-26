9d54b65d84e0b27ce5c915e8b9be22b799f4c5fc
diggzhang <diggzhang@gmail.com>
2015-02-26 22:20:51 +0800

    数据接受文件

--- article.handle.php
+++ article.handle.php
@@ -5,18 +5,20 @@
 	//把传递来的信息入库
 	//print_r($_POST);
 
-	$title 		 = $_POST['title'];
-	$author		 = $_POST['author'];
-	$description = $_POST['description'];
-	$content	 = $_POST['content'];
-
 	//在入库前针对信息校验
 	if( !(isset($_POST['title']) && ( !empty($_POST['title']) ) ) )
 	{
 		echo "<script>alert('Title is NULL'); window.location.href='article.add.php'</script>";		
 	}
+	else
+	{
+		$title = $_POST['title'];
+	}
 
-	$timeline = time();
+	$author		 = $_POST['author'];
+	$description = $_POST['description'];
+	$content	 = $_POST['content'];
+	$timeline 	 = time();
 
 	$insertsql = "insert into article(title, author, description, content, timeline) values('$title', '$author', '$description', '$content', $timeline)";
 	//测试语句是否正确
@@ -31,5 +33,4 @@
 		echo "<script>alert('文章发布失败'); window.location.href='article.add.php'</script>";		
 	}
 
-	echo "world";
 ?>
