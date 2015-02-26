fd39cd4bafd4ad72c54098e90b934616992f3664
diggzhang <diggzhang@gmail.com>
2015-02-26 22:17:40 +0800

    数据库链接文件

--- connect.php
+++ connect.php
@@ -4,23 +4,23 @@
 	//链库
 	//***bug line
 	//if( $con = mysql_connect(HOST, uSERNAME, PASSWORD) )
-	echo "Hello";
+
 	if( $con = mysql_connect(HOST, USERNAME) )
 	{
 		echo mysql_error();	
 	}
+
 	//选库
-	
 	if( mysql_select_db('info') )
 	{
 		echo mysql_error();	
 	}
+
 	//字符集
-	
 	if( mysql_query('set names utf8') )
 	{
 		echo mysql_error();	
 	}
+
 	//查询
-	echo "Hello";
 ?>
